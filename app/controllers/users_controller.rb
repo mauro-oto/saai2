cc685a7aa21035427938954ed4aa444745f10555
boli <wolli.becker@gmail.com>
2013-09-21 22:10:59 -0300

    Ranking: Vinculación completa con la DB.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,10 @@ class UsersController < ApplicationController
   def ranking
   	@usersPuntosPublicos = User.order("puntosPublicos desc").limit(3)
   	@usersPuntosPrivados = User.order("puntosPrivados desc").limit(3)
+  	@usersLists = User.find_by_sql("SELECT users.nombre, count(lists.user_id) AS cantidad 
+  		FROM users INNER JOIN lists ON users.id = lists.user_id 
+  		GROUP BY users.nombre 
+  		ORDER BY cantidad Desc;")
   end
   
 end
