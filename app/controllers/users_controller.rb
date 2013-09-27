69b72524e11c3adb3bfdc65ef9d8accc976b7a96
unknown <mauro.otonelli@gmail.com>
2013-09-26 21:24:54 -0300

    modificacion en vista de lista

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,10 +11,14 @@ class UsersController < ApplicationController
   def ranking
   	@usersPuntosPublicos = User.order("puntosPublicos desc").limit(3)
   	@usersPuntosPrivados = User.order("puntosPrivados desc").limit(3)
-  	@usersLists = User.find_by_sql("SELECT users.nombre, count(lists.user_id) AS cantidad 
-  		FROM users INNER JOIN lists ON users.id = lists.user_id 
-  		GROUP BY users.nombre 
-  		ORDER BY cantidad Desc;")
+  	#@usersLists = User.find_by_sql("SELECT users.nombre, count(lists.user_id) AS cantidad 
+  	#	FROM users INNER JOIN lists ON users.id = lists.user_id 
+  	#	GROUP BY users.nombre 
+  	#	ORDER BY cantidad Desc;")
+    @usersLists = User.select("users.*, COUNT(lists.id) AS count_lists")
+    .joins("LEFT OUTER JOIN lists ON users.id = lists.user_id")
+    .group("users.id")
+    .order("count_lists DESC")
+
   end
-  
 end
