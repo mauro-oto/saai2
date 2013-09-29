cb8dfbde121dafa161e8768ddb95f7811a68cfff
boli <wolli.becker@gmail.com>
2013-09-28 23:19:35 -0300

    Cambios en CSS, cabecera en todas las vistas.

--- db/schema.rb
+++ db/schema.rb
@@ -34,13 +34,11 @@ ActiveRecord::Schema.define(version: 20130921185206) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "fila_id"
-    t.integer  "users_id"
     t.integer  "user_id"
   end
 
   add_index "lists", ["fila_id"], name: "index_lists_on_fila_id", using: :btree
   add_index "lists", ["user_id"], name: "index_lists_on_user_id", using: :btree
-  add_index "lists", ["users_id"], name: "index_lists_on_users_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
