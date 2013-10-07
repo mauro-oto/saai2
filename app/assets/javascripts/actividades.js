c741ba1a1385ece63cad3cf288ef9ef2682be675
boli <wolli.becker@gmail.com>
2013-10-07 18:49:06 -0300

    Armado de fixtures y avance en vistas parciales de actividades.

---
+++ app/assets/javascripts/actividades.js
@@ -0,0 +1,28 @@
+# Place all the behaviors and hooks related to the matching controller here.
+# All this logic will automatically be available in application.js.
+# You can use CoffeeScript in this file: http://coffeescript.org/
+
+var loadPartial=function(name){
+	var xmlhttp;
+	if (window.XMLHttpRequest){
+		// code for IE7+, Firefox, Chrome, Opera, Safari
+		xmlhttp=new XMLHttpRequest();
+	}
+	else{
+		// code for IE6, IE5
+		xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+	}
+	xmlhttp.onreadystatechange=function()
+	{
+		if (xmlhttp.readyState==4 && xmlhttp.status==200)
+		{
+			document.getElementById("partial").innerHTML=xmlhttp.responseText;
+		}
+	}
+	xmlhttp.open("GET", name + ".html.erb",true);
+	xmlhttp.send();
+}
+
+var hello = function(){
+	confirm("Hello");
+};
