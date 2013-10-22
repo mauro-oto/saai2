a4f21636d4667eb9da30b5ebb3ca2524460c87c0
boli <wolli.becker@gmail.com>
2013-10-22 16:45:44 -0300

    Corrección de nombre en ranking

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Saai2::Application.routes.draw do
 
   # Devise
   root :to => "users#menu"
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
