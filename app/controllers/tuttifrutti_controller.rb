bb0518a9aed7de34c7301c11b978e2445e6e9ef1
donboli <wolli.becker@gmail.com>
2013-11-05 21:26:50 -0300

    Tutti Frutti

--- app/controllers/tuttifrutti_controller.rb
+++ app/controllers/tuttifrutti_controller.rb
@@ -1,8 +1,4 @@
 class TuttifruttiController < ApplicationController
   def index
-  	@lists = List.where(user_id: current_user.id).first(5)
-  	for i in 0..@lists.length
-  		@filas[i] = Fila.where(list_id: @lists[i].id)
-  	end
   end
 end
