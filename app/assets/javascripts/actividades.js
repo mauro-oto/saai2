5481ae04692366127ad2667e3f48e04087eeb0cb
donboli <wolli.becker@gmail.com>
2013-11-17 14:06:21 -0300

    Funciona el cambio entre actividades

--- app/assets/javascripts/actividades.js
+++ app/assets/javascripts/actividades.js
@@ -1,41 +1,26 @@
-var loadPartial=function(name){
-	var xmlhttp;
-	if (window.XMLHttpRequest){
-		// code for IE7+, Firefox, Chrome, Opera, Safari
-		xmlhttp=new XMLHttpRequest();
-	}
-	else{
-		// code for IE6, IE5
-		xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-	}
-	xmlhttp.onreadystatechange=function()
-	{
-		if (xmlhttp.readyState==4 && xmlhttp.status==200)
-		{
-			document.getElementById("partial").innerHTML=xmlhttp.responseText;
-		}
-	}
-	xmlhttp.open("GET", name + ".html.erb",true);
-	xmlhttp.send();
-}
-
-var hello = function(){
-	confirm("Hello");
-};
-
-var hidePartials = function(){
-	document.getElementById("proper").style.visibility = "hidden";
-	// document.getElementById("prodem").style.visibility = "hidden";
-	// document.getElementById("propos").style.visibility = "hidden";
-	// document.getElementById("tabpro").style.visibility = "hidden";
-	// document.getElementById("quiz").style.visibility = "hidden";
-}
-
-window.onload=function(){
-	hidePartials();
-};
-
-var loadPartials = function(pagina){
-	hidePartials();
-	document.getElementById(pagina).style.visibility = "visible";
-};
+$(document).ready(function(){
+	$('#properli').click(function(){
+		$('#tabpro').css("display", "none");
+		$('#prodem').css("display", "none");
+		$('#propos').css("display", "none");
+		$('#proper').css("display", "block");
+	});
+	$('#tabproli').click(function(){
+		$('#proper').css("display", "none");
+		$('#prodem').css("display", "none");
+		$('#propos').css("display", "none");
+		$('#tabpro').css("display", "block");
+	});
+	$('#prodemli').click(function(){
+		$('#proper').css("display", "none");
+		$('#tabpro').css("display", "none");
+		$('#propos').css("display", "none");
+		$('#prodem').css("display", "block");
+	});
+	$('#proposli').click(function(){
+		$('#proper').css("display", "none");
+		$('#tabpro').css("display", "none");
+		$('#prodem').css("display", "none");
+		$('#propos').css("display", "block");
+	});
+});
